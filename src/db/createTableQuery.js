9c43640aa10971f1fa001b1617f6d07559aec2cd
reinaldoper <reinaldoper83@gmail.com>
2024-03-10 22:35:22 -0400

    feat: adiciona UNIQUE ao email

--- src/db/createTableQuery.js
+++ src/db/createTableQuery.js
@@ -15,11 +15,11 @@ const createTable = async () => {
   try {
     await pool.query(createTableQuery);
     const clients = [
-      { name: 'Cliente 1', email: 'cliente1@email.com', phone: '111-1111', xCoordinate: -23.5505, yCoordinate: -46.6333 }, // São Paulo
-      { name: 'Cliente 2', email: 'cliente2@email.com', phone: '222-2222', xCoordinate: 40.7128, yCoordinate: -74.0060 }, // Nova Iorque
-      { name: 'Cliente 3', email: 'cliente3@email.com', phone: '333-3333', xCoordinate: 34.0522, yCoordinate: -118.2437 }, // Los Angeles
-      { name: 'Cliente 4', email: 'cliente4@email.com', phone: '444-4444', xCoordinate: 51.5074, yCoordinate: -0.1278 },  // Londres
-      { name: 'Cliente 5', email: 'cliente5@email.com', phone: '555-5555', xCoordinate: 35.6895, yCoordinate: 139.6917 }, // Tóquio
+      { name: 'Mario', email: 'cliente1@email.com', phone: '111-1111', xCoordinate: -23.5505, yCoordinate: -46.6333 }, // São Paulo
+      { name: 'João Paulo', email: 'cliente2@email.com', phone: '222-2222', xCoordinate: 40.7128, yCoordinate: -74.0060 }, // Nova Iorque
+      { name: 'Maria', email: 'cliente3@email.com', phone: '333-3333', xCoordinate: 34.0522, yCoordinate: -118.2437 }, // Los Angeles
+      { name: 'Cleide', email: 'cliente4@email.com', phone: '444-4444', xCoordinate: 51.5074, yCoordinate: -0.1278 },  // Londres
+      { name: 'Jucimara', email: 'cliente5@email.com', phone: '555-5555', xCoordinate: 35.6895, yCoordinate: 139.6917 }, // Tóquio
     ];
     
 
